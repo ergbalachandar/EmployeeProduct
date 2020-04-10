f58094688e65b250f5a271c3dfcc2d34c4cceb6f
Balachandar Gopalan <ergbalachandar@gmail.com>
2020-04-11 00:18:45 +0530

    refactoring autowired

--- EmployeeDetailsProduct/src/main/java/com/employee/product/controller/EmployeeProductController.java
+++ EmployeeDetailsProduct/src/main/java/com/employee/product/controller/EmployeeProductController.java
@@ -1,7 +1,6 @@
 package com.employee.product.controller;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -11,17 +10,15 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.mail.MailSender;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.employee.product.companydetails.request.dto.CompanyDetailsRequestDto;
 import com.employee.product.companydetails.request.dto.LoginDetailsRequestDto;
@@ -60,9 +57,10 @@ public class EmployeeProductController {
 	@Autowired
 	private EmployeeDetailsInterface employeeDetailsInterface;
 
-	/*
-	 * @Autowired private MailSender mailSender;
-	 */
+	
+	  @Autowired 
+	  private MailSender mailSender;
+	 
 
 	/**
 	 * Method to SignUp Company
