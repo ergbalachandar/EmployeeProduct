732d7d9e94bfca0d9d997046e1b599a3ab04a0eb
Balachandar Gopalan <ergbalachandar@gmail.com>
2020-05-21 23:23:03 +0530

    refractoring swaggerconfig

--- EmployeeDetailsProduct/src/main/java/com/employee/product/SwaggerConfig.java
+++ EmployeeDetailsProduct/src/main/java/com/employee/product/SwaggerConfig.java
@@ -6,28 +6,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
 
-import com.google.common.collect.Lists;
-
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.BasicAuth;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.ApiKeyVehicle;
-import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -35,9 +23,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
-	  private static final String BASIC_AUTH = "basicAuth";
-	    private static final String BEARER_AUTH = "Bearer";
-	    public static final String DEFAULT_INCLUDE_PATTERN = "/EProduct/*";
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -66,19 +51,4 @@ public class SwaggerConfig {
     private ApiKey apiKey() {
         return new ApiKey("jwtToken", "Authorization", "header");
     }
- private SecurityContext securityContext() {
-        return SecurityContext.builder()
-            .securityReferences(defaultAuth())
-            .forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN))
-            .build();
-    }
-
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope
-            = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Lists.newArrayList(
-            new SecurityReference("JWT", authorizationScopes));
-    } 
 }
