c17ed879bac4fe0f3f1afa23d569f6c32dff775c
rajesh <raj333921@gmail.com>
2020-10-04 19:28:21 +0530

    New service for user implemented

--- EmployeeDetailsProduct/src/main/java/com/employee/product/controller/PaySlipController.java
+++ EmployeeDetailsProduct/src/main/java/com/employee/product/controller/PaySlipController.java
@@ -1,9 +1,9 @@
 package com.employee.product.controller;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,11 +13,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.employee.product.dao.services.DeleteCompanyService;
+import com.employee.product.dao.services.EmployeeProductService;
 import com.employee.product.dao.services.PaySlipService;
 import com.employee.product.dao.services.UserDetailsImpl;
 import com.employee.product.entity.employeedetails.EmployeeDetails;
+import com.employee.product.entity.employeedetails.EmployeePaySlipDetails;
 import com.employee.product.payslipsdetails.request.dto.EPaySlipReqDto;
+import com.employee.product.payslipsdetails.response.dto.EPaySlipEmpRes;
 import com.employee.product.payslipsdetails.response.dto.EPaySlipResDto;
 import com.employee.product.security.jwt.JwtUtils;
 import com.employee.product.utils.PaySlipsDetailsUtil;
@@ -43,6 +45,10 @@ public class PaySlipController {
 	@Autowired
 	private PaySlipService paySlipService;
 	
+
+	@Autowired
+	private EmployeeProductService employeeProductService;
+	
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/payAdmlist")
 	@ApiOperation(value = "View Payslips", authorizations = { @Authorization(value = "jwtToken") })
@@ -54,12 +60,43 @@ public class PaySlipController {
 	public EPaySlipResDto retrievePayslips(@RequestBody EPaySlipReqDto EPaySlipReqDto)
 			throws Exception {
 		UserDetailsImpl userDetailsImpl = generateUserDetailsFromJWT();
+		if(!"Admin".equals(userDetailsImpl.getUsers().getRole()) || userDetailsImpl.getUsers().getCompanyDetails().getActive() == 0) {
+			throw new Exception("You are not authorized to view");
+		}
 		List<EmployeeDetails> paySlipDetails = paySlipService.retrievePayslipsByCompanyId(userDetailsImpl.getUsers().getCompanyDetails());
 		EPaySlipResDto ePaySlipRes = new EPaySlipResDto();
 		PaySlipsDetailsUtil.mapPaySlipDetails(ePaySlipRes, paySlipDetails);
 		return ePaySlipRes;
 	}
 	
+	@RequestMapping(method = RequestMethod.POST, value = "/paylist")
+	@ApiOperation(value = "View Payslips", authorizations = { @Authorization(value = "jwtToken") })
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved Payslips"),
+			@ApiResponse(code = 401, message = "You are not authorized to Log In"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
+	@ResponseBody
+	public EPaySlipEmpRes retrievePayslipsForEmployee(@RequestBody EPaySlipReqDto EPaySlipReqDto)
+			throws Exception {
+		UserDetailsImpl userDetailsImpl = generateUserDetailsFromJWT();
+		Set<EmployeePaySlipDetails> paySlipDetails = null;
+
+		List<EmployeeDetails> employeeDetailsList = employeeProductService
+				.findbyCompanyDetails(userDetailsImpl.getUsers().getCompanyDetails().getId());
+		for(EmployeeDetails empDet:employeeDetailsList) {
+			System.out.println("---empDet---"+empDet.getEmailId());
+			System.out.println("---UserName---"+userDetailsImpl.getUsers().getUserName());
+			
+			if(empDet.getEmailId().equals(userDetailsImpl.getUsers().getUserName())){
+				paySlipDetails = empDet.getEmployeePaySlipDetails();
+				}
+			}
+		EPaySlipEmpRes ePaySlipEmpRes = new EPaySlipEmpRes();
+		if(null != paySlipDetails && paySlipDetails.size() > 0) {
+			PaySlipsDetailsUtil.mapEmpPaySlipDetails(paySlipDetails, ePaySlipEmpRes);
+		}
+		return ePaySlipEmpRes;
+	}
 	
 	private static UserDetailsImpl generateUserDetailsFromJWT() throws Exception {
 		UserDetailsImpl userDetailsImpl = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication()
